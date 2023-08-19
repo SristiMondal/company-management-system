6db428bb4fb88a2f4c395d565bad1f85117de505
SristiMondal <73815720+SristiMondal@users.noreply.github.com>
2023-08-19 19:21:30 +0530

    Update and rename data.tsx to data.ts

---
+++ src/utils/data.ts
@@ -0,0 +1,19 @@
+export const fetchCompanyData = async () => {
+  try {
+    const response = await fetch(
+      "https://fakerapi.it/api/v1/companies?_quantity=50"
+    );
+
+    // if (!response.ok) {
+    //   throw new Error(`Request failed with status ${response.status}`);
+    // }
+
+    const responseData = await response.json();
+    console.log( responseData?.data?.data,"today")
+    const companies = responseData?.data?.data ?? [];
+    return companies;
+  } catch (error) {
+    console.error("Error fetching company data:", error);
+    return [];
+  }
+};
