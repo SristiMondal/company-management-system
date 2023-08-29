c1b007ceaf85133efa29a90ff52b4476a0d2edd5
sristi <sristi.mondal@quokkalabs.com>
2023-08-29 15:15:00 +0530

    (feat):added login authentication, designed signIn page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,59 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Menu, MenuItem } from "@mui/material";
+import { useDispatch } from "react-redux";
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
+import { setAuthentication } from "../../redux/authSlice";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    handleClose();
+    dispatch(setAuthentication(false));
+  };
+
   return (
     <Box className="header">
       <Box className="logo" onClick={() => navigate("/")}>
         CMS
       </Box>
-      <Box className="profile"></Box>
+      <Box
+        className="profile"
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={(e: any) => handleClick(e)}
+      ></Box>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+        sx={{
+          mt: 1.5,
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
     </Box>
   );
 };
